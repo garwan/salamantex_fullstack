8045f78f565f3551b0af67486856a87d6fe88678
Andrej Skalicky <andrejskalick3@gmail.com>
2022-05-31 14:08:16 +0200

    Redistration validation update added description

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,8 +50,9 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'name' => ['required', 'string', 'max:512'],
+            'email' => ['required', 'string', 'email', 'max:1000', 'unique:users'],
+            'description' => ['string'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
@@ -67,6 +68,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'description' => $data['description'],
             'password' => Hash::make($data['password']),
         ]);
     }
